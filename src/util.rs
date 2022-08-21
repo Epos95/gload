f58acddd4ace9eab8dd595f2deacb88ba13f8ff4
epos95 <maxagnesund95@gmail.com>
2022-08-21 20:44:37 +0200

    adding document for blog and a little comment

--- src/util.rs
+++ src/util.rs
@@ -151,6 +151,7 @@ pub async fn compile(
 ) -> Result<PathBuf, String> {
 
     // need some way to get the "building" part of cargo output
+    // TODO: A way to print the stdout/stderr of cross for debug would be nice
     let s = Command::new("cross")
         .arg("b")
         .arg("--release")
